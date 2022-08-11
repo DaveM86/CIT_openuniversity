52e1307eb25b9e60cd8ebb1ecd60c9cf02304042
DaveM86 <64964552+DaveM86@users.noreply.github.com>
2022-08-11 07:18:47 +0100

    Add files via upload

---
+++ mst124_rootable.py
@@ -0,0 +1,20 @@
+'''
+ROOTABLE - This can be used to find the largest root that is a factor of the target.
+Set the target script will do the rest.
+'''
+import math
+
+def rootable(target, p):
+    for x in range (1,p+1):
+        y = x**2
+        if target % y == 0:
+            print(f'{y} is a root(able) factor of {target}: {x} root '+str(target/y))
+
+def run():
+	t = int(input('target: '))
+	p = math.ceil(t**0.5)
+	rootable(t,p)
+
+if __name__ == '__main__':
+	run()
+
